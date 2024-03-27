d1655a4734977087b5ae0e9d6bb7671fd2132b46
DaUn <85550617+gimdaun@users.noreply.github.com>
2024-03-27 21:45:33 +0900

    Update bedrock_api_rambda.py

--- bedrock_api_rambda.py
+++ bedrock_api_rambda.py
@@ -1,62 +1,29 @@
-import json
-import boto3
+# 엑셀 파일에서 데이터 읽어오기
+plant_data = pd.read_csv('건조에 강한 실내식물.csv')  # 파일명에 맞게 변경 필요
 
+def get_plant_info(query):
+    # 사용자가 입력한 식물 이름으로 검색하여 정보 제공
+    plant_info = plant_data[plant_data['이름'] == query]
 
+    if not plant_info.empty:
+        return plant_info.to_dict(orient='records')
+    else:
+        return None
+        
 def lambda_handler(event, context):
-    print(f">>>>>>>>>>>> ${event}")
+    # 사용자로부터 받은 쿼리를 파싱합니다.
+    body = json.loads(event['body'])
+    query = body['query']
 
-    # Bedrock version check
-    boto3_version = boto3.__version__
-    print(f">>>>>>>>>>>> boto3 version: {boto3_version}")
+    # 쿼리에 일치하는 식물 정보를 검색합니다.
+    plants = get_plant_info(query)
 
-    try:
-        if event["requestContext"]["http"]["method"] == "GET":
-            return done(None, "'/' path로 GET 요청을 하셨군요.")
-
-        # Bedrock runtime
-        bedrock_runtime = boto3.client(
-            service_name="bedrock-runtime", region_name="us-east-1"
-        )
-
-        request_body = json.loads(event.get("body"))
-        prompt = (
-            request_body.get("prompt")
-            if "prompt" in request_body
-            else "Amazon Bedrock이 뭐야? 1문장으로 답변해"
-        )
-        print(f">>>>>>>>>>>> prompt: {prompt}")
-
-        # Anthropic의 Claude 모델 사용
-        # 한국어를 비교적 잘 지원
-        # https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-claude.html
-        body = json.dumps(
-            {
-                "prompt": f"\n\nHuman:{prompt}\n\nAssistant:",
-                "temperature": 0,
-                "top_p": 0.01,
-                "max_tokens_to_sample": 1000,
-            }
-        )
-
-        # buffered
-        response = bedrock_runtime.invoke_model(
-            body=body, modelId="anthropic.claude-v2"
-        )
-        response_body = json.loads(response.get("body").read())
-        model_response = response_body["completion"]
-        print(f">>>>>>>>>>>> model output: {model_response}")
-        return done(None, {"output": model_response})
-    except Exception as e:
-        return done(e, "Error occured!")
-
-
-def done(err, res):
-    if err:
-        print(f"!!!!!!!!!!!!{err}")
-
+    # 결과 반환
     return {
-        "statusCode": "400" if err else "200",
-        # 한글 깨짐을 방지하기 위해 ensure_ascii 옵션 추가
-        "body": json.dumps(res, ensure_ascii=False),
-        "headers": {"Content-Type": "application/json"},
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',  # CORS 설정
+        },
+        'body': json.dumps({"plants": plants if plants else []})
     }
